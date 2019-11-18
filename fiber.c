0c7e7dff3f2d39625c7836a6d32f56b4707ab494
GuilhermeBartasson <44179839+GuilhermeBartasson@users.noreply.github.com>
2019-11-18 10:39:59 -0300

    Fase inicial da fiber_create()

--- fiber.c
+++ fiber.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <ucontext.h>
 #include <stdlib.h>
-#include <pthread.h>
 
 /*  
     Implementação de threads em user-level(fibers) no modelo M para 1(M threads user-level 
@@ -9,6 +8,9 @@
 
     by Guilherme Bartasson, Diego Batistuta e Vitor Teixeira, 2019
 */
+
+// Pilha de 64kB
+#define FIBER_STACK 1024*64
 
 typedef int fiber_t; // tipo para ID de fibers
 
@@ -19,18 +21,47 @@ typedef struct {
     fiber_t * next;
     fiber_t * prev;
     fiber_t * fiber_list;
-    fiber_t * this;
+    ucontext_t context;
     fiber_t fiberId;
-    fiber_t state;
-}fiber;
+}fiber_struct;
 
 /*
-    Lista que armazenará as fibers criadas pela biblioteca
+    Struct da lista que armazenará as fibers criadas pela biblioteca
 */
 typedef struct {
-    fiber * threads;
+    fiber_struct * fibers;
 }fiber_list;
 
+// Lista global que armazenará as fibers
+fiber_list f_list;
+
+/*
+    Insere uma fiber na última posição da lista de fibers
+*/
+void pushFiber(fiber_struct * fiber) {
+    // Caso não haja nenhuma fiber na lista
+    if (f_list.fibers = NULL) {
+        f_list.fibers = fiber;
+
+        fiber->prev = NULL;
+        fiber->next = NULL;
+        fiber->fiber_list = &f_list;
+    } else { // Caso contrário
+        fiber_struct * f_aux1 = f_list->fibers;
+        fiber_t * f_aux2 = f_aux1->next;
+
+        // Procurando o último elemento da lista de fibers
+        while (f_aux2 != NULL) {
+            f_aux2 = f_aux2->next;
+        }
+
+        // Inserindo a nova fiber no fim da lista
+        f_aux2->next = fiber;
+        fiber->prev = f_aux2;
+        fiber->next = NULL;
+        fiber->fiber_list = &f_list;
+    }
+}
 
 /*
     fiber_create
@@ -41,7 +72,38 @@ typedef struct {
 
 */
 int fiber_create(fiber_t *fiber, void *(*start_routine) (void *), void *arg) {
-    pthread_create(fiber, NULL, start_routine, arg);
+
+    // Variável que irá armazenar a nova fiber 
+    ucontext_t fiber;
+
+    // Struct que irá armazenar a nova fiber
+    fiber_struct * f_struct;
+
+    // Obtendo o contexto atual e armazenando-o na variável fiber
+    getcontext(&fiber);
+
+    // Modificando o contexto para um nova pilha
+    fiber.uc_link = 0;
+    fiber.uc_stack.ss_sp = malloc( FIBER_STACK );
+    fiber.uc_stack.ss_size = FIBER_STACK;
+    fiber.uc_stack.ss_flags = 0;        
+    if (fiber.uc_stack.ss_sp == 0 ) {
+        perror("malloc: Não foi possível alocar a pilha");
+        exit(1);
+    }
+
+    // Criando a fiber propriamente dita
+    makecontext(&fiber, start_routine, arg);
+
+    // Inicializando a struct que armazena a fiber recem criada
+    f_struct->context = &fiber;
+    f_struct->fiberId = fiber;
+    f_struct->prev = NULL;
+    f_struct->next = NULL;
+    f_struct->fiber_list = NULL;
+
+    // Inserindo a nova fiber na lista de fibers
+    pushFiber(f_struct);
 }
 
 /*
