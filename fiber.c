381c4ed8a081103fff80233161cb6ebf6c6f4387
Vitor-Tx <49378292+Vitor-Tx@users.noreply.github.com>
2019-12-01 20:25:48 -0300

    Update fiber.c

--- fiber.c
+++ fiber.c
@@ -20,11 +20,25 @@
 typedef unsigned int fiber_t; // tipo para ID de fibers
 
 /*
-    Struct de uma fiber
-    status: 
-    -  1: ativa
-    -  0: esperando outra fiber com join
-    - -1: terminada
+    Struct de uma fiber.
+    Atributos:
+
+    - next e prev: ponteiros para outras estruturas
+      de fibers para que seja feita uma lista circular.
+
+    - context: ponteiro para a estrutura ucontext_t
+      que contém o contexto da fiber.
+
+    - status: representa o estado atual da fiber: 
+        quando é  1: fiber ativa/pronta para ser executada;
+        quando é  0: esperando outra fiber com join;
+        quando é -1: fiber terminada;
+    
+    - fiberId: id inteiro da fiber. Nunca é negativo.
+      o id da thread principal é 0.
+    
+    - joinFiber: ponteiro para a fiber que essa fiber 
+      está esperando.
 */
 typedef struct Fiber{
     struct Fiber * next;        // Próxima fiber da lista
@@ -36,7 +50,17 @@ typedef struct Fiber{
 }Fiber;
 
 /*
-    Struct da lista que armazenará as fibers criadas pela biblioteca
+    Struct da lista que armazenará as fibers criadas pela biblioteca.
+    Atributos:
+
+    - fibers: ponteiro para a "cabeça" da lista circular de fibers.
+      A thread principal sempre será a "cabeça" da lista.
+
+    - currentFiber: ponteiro para a fiber que está sendo executada 
+      no momento.
+
+    - nFibers: inteiro que contém o número de fibers na lista. A 
+    thread principal faz parte da contagem.
 */
 typedef struct FiberList{
     Fiber * fibers;             // Lista de fibers
@@ -44,7 +68,7 @@ typedef struct FiberList{
     int nFibers;                // Quantidade de fibers na lista
 }FiberList;
 
-// Contexto do escalonador e do processo pai
+// Contexto para a função de escalonamento e da thread principal
 ucontext_t schedulerContext, parentContext;
 
 // Lista global que armazenará as fibers
@@ -66,7 +90,19 @@ void getnFibers() {
 }
 
 /*
-    Escalonador de fibers
+    fiberScheduler
+    --------------
+
+    Toda vez que é chamada, verifica o status da fiber logo após a atual.
+
+    Caso o status da próxima fiber não seja 1, o loop é executado e continuará
+    procurando fibers disponíveis para execução. Fibers com status -1 nunca
+    serão executadas, mas fibers com status 0 poderão ser executadas, caso
+    a fiber que elas estiverem esperando esteja terminada(status -1), e terão
+    seu status atualizado para 1 novamente.
+
+    Caso a fiber tenha status 1, a fiber atual terá seu contexto salvo, e o 
+    contexto atual será alterado para a fiber com status 1 encontrada.
 */
 void fiberScheduler() {
     Fiber * currentFiber = (Fiber *) f_list->currentFiber;
@@ -100,7 +136,7 @@ void fiberScheduler() {
 }
 
 /*
-    Funçaõ responsável por iniciar o escalonado
+    Função responsável por iniciar o escalonado
 */
 void startFibers() {
     struct sigaction sa;
@@ -108,7 +144,7 @@ void startFibers() {
     int seconds = 0;
     int microSeconds = 500;
 
-    // Obtendo o contexto da primeira fiber (que não é o processo pai)
+    // Obtendo o contexto da primeira fiber (que não é a thread principal)
     Fiber * aux = (Fiber *) f_list->fibers;
     aux = (Fiber *) aux->next;
 
@@ -165,70 +201,75 @@ int fiberSwap(fiber_t fiberId) {
 }
 
 /*
+    initFiberList
+    -------------
+
     Função responsável por inicializar a lista de fibers caso ela não tenha
-    sido inicializada ainda
+    sido inicializada ainda.
+    Ela também insere a estrutura da thread principal como a cabeça da lista,
+    e cria o contexto para a função FiberScheduler.
 */
 int initFiberList() {
-    if (f_list == NULL) {
-        f_list = (FiberList *) malloc(sizeof(FiberList));
-        f_list->fibers = NULL;
-        f_list->nFibers = 0;
-        f_list->currentFiber = NULL;
+    
+    f_list = (FiberList *) malloc(sizeof(FiberList));
+    f_list->fibers = NULL;
+    f_list->nFibers = 0;
+    f_list->currentFiber = NULL;
+    
+    // Criando a estrutura de fiber para a thread principal
+    Fiber * parentFiber = (Fiber *) malloc(sizeof(Fiber));
+
+    // Inicializando a estrutura da thread principal
+    parentFiber->context = &parentContext;
+    parentFiber->fiberId = 0;
+    parentFiber->prev = NULL;
+    parentFiber->next = NULL; 
+    parentFiber->status = 1;
+
+    // Adicionado a estrutura da thread principal como o primeiro elemento da lista
+    f_list->fibers = (Fiber *) parentFiber;
+
+    // Incrementando o número de fibers da lista
+    f_list->nFibers++;
+
+    // Definindo a thread principal como fiber atual
+    f_list->currentFiber = (Fiber *) parentFiber;
+
+    // Obtendo um contexto para o escalonador
+    getcontext(&schedulerContext);
+    
+    // Limpamdo o contexto recebido para o escalonador
+    schedulerContext.uc_link = &parentContext;
+    schedulerContext.uc_stack.ss_sp = malloc(FIBER_STACK);
+    schedulerContext.uc_stack.ss_size = FIBER_STACK;
+    schedulerContext.uc_stack.ss_flags = 0;        
+    if (schedulerContext.uc_stack.ss_sp == 0 ) {
+        printf("malloc: Não foi possível alocar a pilha para o escalonador");
+        return 1;
     }
 
-    // Caso não haja nenhuma fiber na lista
-    if (f_list->fibers == NULL) {
+    // Criando o contexto do escalonador própriamente dito
+    makecontext(&schedulerContext, fiberScheduler, 1, NULL);
 
-        // Criando a fiber do processo pai
-        Fiber * parentFiber = (Fiber *) malloc(sizeof(Fiber));
-
-        // Inicializando a fiber do processo pai
-        parentFiber->context = &parentContext;
-        parentFiber->fiberId = 0;
-        parentFiber->prev = NULL;
-        parentFiber->next = NULL; 
-        parentFiber->status = 1;
-
-        // Adicionado a fiber do processo pai como o primeiro elemento da lista
-        f_list->fibers = (Fiber *) parentFiber;
-
-        // Incrementando o número de fiber
-        f_list->nFibers++;
-
-        // Definindo o processo pai como fiber atual
-        f_list->currentFiber = (Fiber *) parentFiber;
-
-        // Obtendo um contexto para o escalonador
-		getcontext(&schedulerContext);
-        
-        // Limpamdo o contexto recebido para o escalonador
-        schedulerContext.uc_link = &parentContext;
-        schedulerContext.uc_stack.ss_sp = malloc(FIBER_STACK);
-        schedulerContext.uc_stack.ss_size = FIBER_STACK;
-        schedulerContext.uc_stack.ss_flags = 0;        
-        if (schedulerContext.uc_stack.ss_sp == 0 ) {
-            printf("malloc: Não foi possível alocar a pilha para o escalonador");
-            return 1;
-        }
-
-        // Criando o contexto do escalonador própriamente dito
-        makecontext(&schedulerContext, fiberScheduler, 1, NULL);
-    }
 	return 0;
 }
 
 /*
-    Insere uma fiber na última posição da lista de fibers
+    pushFiber
+    ---------
+
+    Insere a fiber recebida pela função na lista de fibers.
 */
 void pushFiber(Fiber * fiber) {
     
-    // Iniciando a lista de fibers
-    initFiberList();
+    // Iniciando a lista de fibers, caso seja null
+    if(f_list == NULL)
+        initFiberList();
     
     // Obtendo a primeira fiber da lista (Processo pai)
     Fiber * firstFiber = (Fiber *) f_list->fibers;
 
-    // Caso só haja a fiber do processo pai na lista
+    // Caso só haja a fiber da thread principal na lista
     if (f_list->nFibers == 1) {
         firstFiber->next = (Fiber *) fiber;
         firstFiber->prev = (Fiber *) fiber;
@@ -409,22 +450,22 @@ int fiber_destroy(fiber_t fiber){
     fiber_exit
     ----------
 
-    Para a execução da thread atual.
+    Para a execução da fiber atual e altera seu status para -1.
+    Com seu status -1, essa fiber nunca mais será executada.
+    Logo após isso, o escalonador é chamado.
 
 */
 void fiber_exit(void *retval){
 
-    Fiber * currentFiber = (Fiber *) f_list->currentFiber;
-
-    // Caso a fiber atual seja o processo pai
-    if (currentFiber->fiberId == 0){
+    // Caso a fiber atual seja a thread principal
+    if (f_list->currentFiber->fiberId == 0){
         printf("Uso inadequado de fiber_exit(). Caso queira terminar o programa, use exit() em vez disso.\n");
         return;
     }
 
     // Definindo status da fiber atual como terminada
-    currentFiber->status = -1;
+    f_list->currentFiber->status = -1;
 
-    // Setando o contexto do escalonador
-    setcontext(&schedulerContext);
+    // Chamando o escalonador(essa fiber não irá voltar)
+    fiberScheduler();
 }
